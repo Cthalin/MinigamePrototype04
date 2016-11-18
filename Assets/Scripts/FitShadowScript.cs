68a2050d0bf6e39d48154d5944adf1c15bd712fa
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-18 17:05:55 +0100

    rotating evaluation script testing

--- Assets/Scripts/FitShadowScript.cs
+++ Assets/Scripts/FitShadowScript.cs
@@ -6,23 +6,43 @@ public class FitShadowScript : MonoBehaviour {
     public GameObject RotObj;
     public GameObject Shadow;
     private Quaternion _objRotation;
-    private Quaternion _shadowRotation;
+    private float _shadowRotation;
     public GameObject EndScreen;
+
+    void Start()
+    {
+        _shadowRotation = Shadow.transform.eulerAngles.z;
+    }
 
     void Update () {
 
         if (Input.GetMouseButtonUp(0))
         {
-            if (RotObj.transform.rotation.z >= (Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) && RotObj.transform.rotation.z <= (Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
+            //if (RotObj.transform.rotation.z >= (Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) && RotObj.transform.rotation.z <= (Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
+            //{
+            //    EndScreen.SetActive(true);
+            //}
+            //else if (RotObj.transform.rotation.z <= -(Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) && RotObj.transform.rotation.z >= -(Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
+            //{
+            //    EndScreen.SetActive(true);
+            //}
+
+            if (RotObj.transform.rotation.z <= (Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) || RotObj.transform.rotation.z >= (Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
             {
-                EndScreen.SetActive(true);
+                EndScreen.SetActive(false);
             }
-            else if (RotObj.transform.rotation.z <= -(Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) && RotObj.transform.rotation.z >= -(Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
+
+            if (RotObj.transform.eulerAngles.z <= _shadowRotation + 10 && RotObj.transform.eulerAngles.z >= -90 + 10)
             {
                 EndScreen.SetActive(true);
             }
 
-            if (RotObj.transform.rotation.z <= (Shadow.transform.rotation.z - Shadow.transform.rotation.z / 20) || RotObj.transform.rotation.z >= (Shadow.transform.rotation.z + Shadow.transform.rotation.z / 20))
+            //if (RotObj.transform.rotation.z >= _shadowRotation-10 && RotObj.transform.rotation.z <= _shadowRotation+10)
+            //{
+            //    EndScreen.SetActive(true);
+            //}
+
+            if (RotObj.transform.rotation.z <= _shadowRotation - 10 || RotObj.transform.rotation.z >= _shadowRotation + 10)
             {
                 EndScreen.SetActive(false);
             }
