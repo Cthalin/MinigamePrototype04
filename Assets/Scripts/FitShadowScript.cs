de95c765366ce8fb3a5a92b7cca3688ba1be648d
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-21 15:50:34 +0100

    added UI for _dots

--- Assets/Scripts/FitShadowScript.cs
+++ Assets/Scripts/FitShadowScript.cs
@@ -5,53 +5,42 @@ public class FitShadowScript : MonoBehaviour {
 
     public GameObject RotObj;
     public GameObject Shadow;
-    //private float _shadowRotation;
-    //private Transform _shadowTransform;
     public GameObject EndScreen;
     private Vector3 _shadowForward;
     private Vector3 _shadowUp;
     private float _dotForward;
     private float _dotUp;
+    public UIScript UIScript;
 
     void Start()
     {
-        //_shadowRotation = Shadow.transform.eulerAngles.z;
-        //_shadowTransform = Shadow.transform;
         _shadowForward = Shadow.transform.forward;
         _shadowUp = Shadow.transform.up;
+        UIScript = GetComponent<UIScript>();
     }
 
     void Update () {
 
+        _dotForward = Vector3.Dot(RotObj.transform.forward, _shadowForward);
+        _dotUp = Vector3.Dot(RotObj.transform.up, _shadowUp);
+
+        UIScript.SetDotForward(_dotForward);
+        UIScript.SetDotUp(_dotUp);
+
         if (Input.GetMouseButtonUp(0))
         {
-            //if (RotObj.transform.eulerAngles.z <= _shadowRotation + 10 && RotObj.transform.eulerAngles.z >= -90 + 10)
-            //{
-            //    _fits = true;
-            //    EndScreen.SetActive(true);
-            //}
-            //else if (!(RotObj.transform.eulerAngles.z <= _shadowRotation + 10 && RotObj.transform.eulerAngles.z >= -90 + 10))
-            //{
-            //    _fits = false;
-            //    print("Möp");
-            //    EndScreen.SetActive(false);
-            //}
-
-            _dotForward = Vector3.Dot(RotObj.transform.forward, _shadowForward);
-            _dotUp = Vector3.Dot(RotObj.transform.up, _shadowUp);
-
-            print(_dotForward);
-            print(_dotUp);
-
             if (_dotForward <= 1.02 && _dotForward >= 0.98 && _dotUp <= 1.02 && _dotUp >= 0.98)
             {
-                print("Dot fit");
                 EndScreen.SetActive(true);
             } else if (!(_dotForward <= 1.02 && _dotForward >= 0.98 && _dotUp <= 1.02 && _dotUp >= 0.98))
             {
-                print("Dot not fit");
                 EndScreen.SetActive(false);
             }
         }        
+    }
+
+    public float GetDotForward()
+    {
+        return _dotForward;
     }
 }
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour {
+
+    public Text DotForward;
+    public Text DotUp;
+
+    public void SetDotForward(float _dotForward)
+    {
+        DotForward.text = "_dotForward: " + _dotForward;
+    }
+    public void SetDotUp(float _dotUp)
+    {
+        DotUp.text = "_dotUp: " + _dotUp;
+    }
+}
