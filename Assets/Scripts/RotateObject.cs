15e2769cde50f61ec9b25d2a4b52363ae6820af8
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-21 17:52:50 +0100

    added deltatime to rotation

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -4,15 +4,25 @@ using System.Collections;
 public class RotateObject : MonoBehaviour {
 
     public float RotSpeed = 10;
+    public GameObject LeftButton;
+    public GameObject RightButton;
 
     void OnMouseDrag()
     {
         float rotX = Input.GetAxis("Mouse X") * RotSpeed * Mathf.Deg2Rad;
         float rotY = Input.GetAxis("Mouse Y") * RotSpeed * Mathf.Deg2Rad;
 
-        //transform.RotateAround(Vector3.up, -rotX);
-        //transform.RotateAround(Vector3.right, rotY);
-        transform.Rotate(Vector3.up, -rotX * 40, Space.World);
-        transform.Rotate(Vector3.right, rotY * 40, Space.World);
+        transform.Rotate(Vector3.up * Time.deltaTime, -rotX * 40, Space.World);
+        transform.Rotate(Vector3.right * Time.deltaTime, rotY * 40, Space.World);
+    }
+
+    public void RotateZLeft()
+    {
+        transform.Rotate(Vector3.forward * Time.deltaTime, Space.World);
+    }
+
+    public void RotateZRight()
+    {
+
     }
 }
