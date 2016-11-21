5c57af1d43761bd4edce1986ae67f7fac5359afb
Erik Slowikowski <e.slowikowski@bigpoint.net>
2016-11-21 18:26:58 +0100

    added Z rotation buttons

--- Assets/Scripts/RotateObject.cs
+++ Assets/Scripts/RotateObject.cs
@@ -1,11 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 public class RotateObject : MonoBehaviour {
 
     public float RotSpeed = 10;
+    public float ZMultiplier = 2;
     public GameObject LeftButton;
     public GameObject RightButton;
+    private bool _rotateLeftButtonPressed = false;
+    private bool _rotateRightButtonPressed = false;
 
     void OnMouseDrag()
     {
@@ -16,13 +21,31 @@ public class RotateObject : MonoBehaviour {
         transform.Rotate(Vector3.right * Time.deltaTime, rotY * 40, Space.World);
     }
 
-    public void RotateZLeft()
+    void Update()
     {
-        transform.Rotate(Vector3.forward * Time.deltaTime, Space.World);
+        if (_rotateLeftButtonPressed)
+        {
+            transform.Rotate(Vector3.forward * Time.deltaTime * RotSpeed * ZMultiplier, Space.World);
+        } else if (_rotateRightButtonPressed)
+        {
+            transform.Rotate(Vector3.back * Time.deltaTime * RotSpeed * ZMultiplier, Space.World);
+        }
     }
 
-    public void RotateZRight()
+    public void onPointerDownRotateLeftButton()
     {
-
+        _rotateLeftButtonPressed = true;
+    }
+    public void onPointerUpRotateLeftButton()
+    {
+        _rotateLeftButtonPressed = false;
+    }
+    public void onPointerDownRotateRightButton()
+    {
+        _rotateRightButtonPressed = true;
+    }
+    public void onPointerUpRotateRightButton()
+    {
+        _rotateRightButtonPressed = false;
     }
 }
